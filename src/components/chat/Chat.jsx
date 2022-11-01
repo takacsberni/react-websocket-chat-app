29f97b54a958b52ae2ef670d162def5c6cd9fd9e
takacsberni <takacsberni@gmail.com>
2022-11-01 11:21:52 +0100

    feat: make Chat component to be able to display a chat message

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,12 +1,36 @@
-import {Container, Paper} from "@mui/material";
-import {Fragment} from "react";
+import {Box, Container, Divider, Grid, List, ListItem, ListItemText, Paper, Typography} from "@mui/material";
+import {Fragment, useState} from "react";
 
 export default function Chat(){
+
+    const [chatMessages, setChatMessages] = useState([]);
+    const listChatMessages = chatMessages.map( (chatMessageDto, index) =>
+        <ListItem key={index}>
+            <ListItemText primary={`${chatMessageDto.user}: ${chatMessageDto.message}`} />
+        </ListItem>
+    );
+
     return (
         <Fragment>
             <Container>
                 {/*Container centers everything horizontally*/}
                 <Paper elevation={5}>
+                    <Box p={3}>
+                        <Typography variant="h4" gutterBottom>
+                            Happy Chatting!
+                        </Typography>
+                        <Divider />
+                        <Grid container spacing={4} alignItems="center">
+                            <Grid item>
+                                <List>
+                                    {listChatMessages}
+                                </List>
+                            </Grid>
+                            <Grid item></Grid>
+                            <Grid item></Grid>
+                            <Grid item></Grid>
+                        </Grid>
+                    </Box>
 
                 </Paper>
             </Container>
