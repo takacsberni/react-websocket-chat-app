de1752e66e1afc389dd2d2b90d0f7be00905408b
takacsberni <takacsberni@gmail.com>
2022-11-01 09:06:30 +0100

    feat: first commit

---
+++ src/components/bar/Bar.jsx
@@ -0,0 +1,22 @@
+import {Fragment} from "react";
+import {AppBar, Box, Toolbar, Typography} from "@mui/material";
+import ChatIcon from '@mui/icons-material/Chat';
+
+export default function Bar() {
+    return (
+        <Fragment>
+            <Box mb={4}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Box>
+                            <ChatIcon />
+                        </Box>
+                        <Typography variant="h6">
+                            React - Websocket Chat Application
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </Fragment>
+    )
+}
